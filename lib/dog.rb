2cd64e30638fa62082a7ce82ac20f90a99e224a7
Brian Rosengarten <barosengarten@gmail.com>
2019-01-24 05:57:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,6 +26,44 @@ attr_reader :id
   end
 
 
+  # def save
+  #   if self.id
+  #     self.update
+  #   else
+  #     sql = <<-SQL
+  #       INSERT INTO students (name, grade)
+  #       VALUES (?, ?)
+  #     SQL
+  # 
+  #     DB[:conn].execute(sql, self.name, self. grade)
+  # 
+  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  #   end
+  # end
+  # 
+  # def self.create(name, grade)
+  #   new_student = Student.new(name, grade)
+  #   new_student.save
+  #   new_student
+  # end
+  # 
+  def self.new_from_db(row)
+    new_dog = Dog.new(row[0], row[1], row[2])
+    new_dog
+  end
+  # 
+  # def self.find_by_name(name)
+  #   sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+  #   DB[:conn].execute(sql, name).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
+  # 
+  # def update
+  #   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  #   DB[:conn].execute(sql, self.name, self.grade, self.id)
+  # end
+
 
 
 end
