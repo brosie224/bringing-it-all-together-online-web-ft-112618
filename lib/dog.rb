158ac8ee8abc37edf4a4505053d682c020336dcd
Brian Rosengarten <barosengarten@gmail.com>
2019-01-24 05:51:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,14 @@ attr_reader :id
     @breed = breed
   end
 
-
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
