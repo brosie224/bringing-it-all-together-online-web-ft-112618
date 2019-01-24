8f46ca78fd5e786ecedd5f00714e16cf8bdb16dc
Brian Rosengarten <barosengarten@gmail.com>
2019-01-24 06:00:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,10 +59,10 @@ attr_reader :id
     end.first
   end
 
-  # def update
-  #   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-  #   DB[:conn].execute(sql, self.name, self.grade, self.id)
-  # end
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 
 
