0e476ff10ea90253b3be710bf335c354e20547a2
Brian Rosengarten <barosengarten@gmail.com>
2019-01-24 05:46:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,16 @@ class Dog
 attr_accessor :name, :breed
 attr_reader :id
 
-  def initialize(name:, breed:)
-    @id = nil
-    @name = name
-    @breed = breed
+  # def initialize(name:, breed:)
+  #   @id = nil
+  #   @name = name
+  #   @breed = breed
+  # end
+
+  def initialize(attributes)
+    #id: nil, name:, breed:
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
 
 end
